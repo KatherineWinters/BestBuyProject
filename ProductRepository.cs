2a472dee8d30ab86a9540a1160eba49745188092
Katherine Winters <goble.katherine@gmail.com>
2022-01-26 10:36:41 -0600

    inital commit

---
+++ ProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+        
+        public ProductRepository
+    }
+}
