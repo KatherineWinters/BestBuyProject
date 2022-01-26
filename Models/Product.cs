53badc869c2cd858a0d67352b98dbdaf1479b883
Katherine Winters <goble.katherine@gmail.com>
2022-01-26 15:04:25 -0600

    initial commit

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,20 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ASPNET.Models
 {
     public class Product
     {
-
-        public Product()
-        {
-        }
-
         public int ProductID { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
         public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
     }
-
 }
